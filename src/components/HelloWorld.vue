e1ba0e3168c227151013920daf0a2496881b4004
周星星 <564751169@qq.com>
2024-07-29 12:43:28 +0800

    新增本地的模型文件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,17 @@
 <template>
-  <canvas ref="liveCanvas" style="width: 100%; height: 100%"></canvas>
+  <div>
+    <div class="buttons-group-wrap">
+      <button class="btn" @click="handleMotion('Tap@Body')">Tap@Body</button>
+      <button class="btn" @click="handleMotion('Flick')">Flick</button>
+      <button class="btn" @click="handleMotion('FlickDown')">FlickDown</button>
+      <button class="btn" @click="handleMotion('Tap')">Tap</button>
+      <button class="btn" @click="handleMotion('Tap@Body')">Tap@Body</button>
+      <button class="btn" @click="handleMotion('Flick@Body')">
+        Flick@Body
+      </button>
+    </div>
+    <canvas ref="liveCanvas"></canvas>
+  </div>
 </template>
 
 <script>
@@ -27,15 +39,21 @@ export default {
     //2.https://cdn.jsdelivr.net/gh/Eikanya/Live2d-model/Live2D/Senko_Normals/senko.model3.json
 
     model = await Live2DModel.from(
-      "https://cdn.jsdelivr.net/gh/Eikanya/Live2d-model/Live2D/Senko_Normals/senko.model3.json"
+      "./hiyori_free_zh/runtime/hiyori_free_t08.model3.json"
     );
     app.stage.addChild(model);
-    model.scale.set(0.3); // 调整缩放比例，一般原始资源尺寸非常大，需要缩小
+    model.scale.set(0.2); // 调整缩放比例，一般原始资源尺寸非常大，需要缩小
     // 点击live2d人物不同部位时的回调，只有定义了可点击区域的人物才会收到回调
     model.on("hit", (hitAreas) => {
       // hitAreas 包含点击的区域和鼠标坐标
       console.log(hitAreas);
     });
+  },
+  methods: {
+    //执行动作
+    handleMotion(motionName) {
+      model.motion(motionName);
+    },
   },
   beforeUnmount() {
     model?.destroy();
@@ -44,4 +62,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.btn {
+  margin-right: 20px;
+}
+</style>
