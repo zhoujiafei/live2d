770e6561c3b6accb61ec7953854a35ca8679198f
周星星 <564751169@qq.com>
2024-07-29 11:17:09 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -23,8 +23,11 @@ export default {
       backgroundAlpha: 0,
     });
     // 打包后live2d资源会出现在dist/下，这里用相对路径就能引用到了
+    //1.https://cdn.jsdelivr.net/gh/guansss/pixi-live2d-display/test/assets/haru/haru_greeter_t03.model3.json
+    //2.https://cdn.jsdelivr.net/gh/Eikanya/Live2d-model/Live2D/Senko_Normals/senko.model3.json
+
     model = await Live2DModel.from(
-      "https://cdn.jsdelivr.net/gh/Eikanya/Live2d-model/Live2D/Senko_Normals/senko.model3.json"
+      "https://cdn.jsdelivr.net/gh/guansss/pixi-live2d-display/test/assets/haru/haru_greeter_t03.model3.json"
     );
     app.stage.addChild(model);
     model.scale.set(0.3); // 调整缩放比例，一般原始资源尺寸非常大，需要缩小
